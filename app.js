883f566250cedf510c3181df4faabd304ead7917
bilawal.aziz <bilawal.aziz@gmail.com>
2017-08-23 19:25:53 -0700

    modified script

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ app.use(restify.fullResponse());
 app.use(restify.bodyParser());
 app.use(restify.queryParser());
 
-app.listen(config.port, function() {
-	console.log('server listening on port number', config.port);
-	
+app.set('port', (process.env.PORT || config.port));
+
+app.listen(app.get('port'), function(){
+	console.log('Server started on port '+app.get('port'));
 });
 var routes = require('./routes')(app);
 var passport = require('./passport')(app);
+
